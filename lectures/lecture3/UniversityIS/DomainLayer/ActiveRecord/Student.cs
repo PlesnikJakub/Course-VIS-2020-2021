8525c5aeb5abc9ddf538a4402706e920aecc230e
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-10-14 14:53:18 +0200

    Add ActiveRecord

---
+++ lectures/lecture3/UniversityIS/DomainLayer/ActiveRecord/Student.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DomainLayer.ActiveRecord
+{
+    public class Student
+    {
+        // Data
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        private IEnumerable<double> Grades = new List<double>(); 
+
+        // Behavior
+        public double ComputeFinalGrade()
+        {
+            // TODO
+            // Compute grade for student
+            return 0.0;
+        }
+
+        public bool HasRightsToScholarship()
+        {
+            // TODO
+            // Compute if student has rights to access scholarship.
+            return true;
+        }
+
+        // Data Access
+        public static Student GetStudentByLogin(string login)
+        {
+            // Call to DB HERE
+            // Option 1. Directly call SQL
+            // Option 2. Call some class from DataLayer project(TDG)
+
+            return new Student();
+        }
+
+    }
+}
